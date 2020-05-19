9d281e42b27b8dc40dfbc8f6a7d422f8ea2b8389
程序员小小叶 <3203636266@qq.com>
2020-05-19 19:06:29 +0800

    更新布局及资源
    1.支持滑动动画；

---
+++ app/src/main/java/com/codermrye/softsearchview/MainActivity.java
@@ -0,0 +1,60 @@
+package com.codermrye.softsearchview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+    private ViewUpSearch search;
+    private boolean isExpand;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        search = (ViewUpSearch) findViewById(R.id.search);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        isExpand = true;
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int firstPosition = mLayoutManager.findFirstCompletelyVisibleItemPosition();
+                if (firstPosition != 0 && isExpand) {
+                    search.updateShow(false);
+                    isExpand = false;
+                }
+                if (firstPosition == 0 && !isExpand) {
+                    search.updateShow(true);
+                    isExpand = true;
+                }
+            }
+        });
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] myDataset = {"AndroidBeta", "Cupcake", "Donut", "Froyo", "Gingerbread", "Honeycomb",
+                "Ice Cream Sandwich", "Jelly Bean", "KitKat"};
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
